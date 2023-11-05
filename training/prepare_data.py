7274705461825f9d157f6120bfde80dd3dc5a996
Andreas Sünder <asuender@student.tgm.ac.at>
2023-11-05 13:56:01 +0100

    Update prepare_date

--- training/prepare_data.py
+++ training/prepare_data.py
@@ -1,48 +1,48 @@
 import logging
 import sys
 from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 from argparse import ArgumentParser
 
-import pandas as pd
 from datasets import load_dataset
 
 from nltk.parse import CoreNLPParser
 
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO, datefmt='%H:%M:%S%p')
-
-CORENLP_SERVER_URL = 'http://localhost:9000'
-OUTPUT_FILE = 'wiki_tokenized.pickle'
-
-parser = ArgumentParser()
-parser.add_argument('subset', type=str, help='Subset of the wikipedia dataset to use (e.g. "en", "simple")')
-parser.add_argument('--server', type=str, default=CORENLP_SERVER_URL, help='URL of the CoreNLP server to use (default: %(default)s)')
-
-def load_data(subset: str):
+def tokenize_wiki(subset: str, parser_url: str):
   logging.info('Loading data')
-  return load_dataset('wikipedia', f'20220301.{subset}', split='train').to_pandas().drop(columns=['url'])
+  data = load_dataset('wikipedia', f'20220301.{subset}', split='train').to_pandas().drop(columns=['url'])
 
-def tokenize(data: pd.DataFrame, server_url: str):
-  parser = CoreNLPParser(url = server_url)
+  parser = CoreNLPParser(url = parser_url)
   
   logging.info('Tokenizing data')
-  data['text'] = data['text'].str.lower().replace('\n', ' ')
+  data['title'] = data['title'].str.lower()
+  data['text'] = data['text'].str.lower()
 
-  for i in tqdm(range(len(data))):
-    try:
-      data['text'][i] = ' '.join(parser.tokenize(data['text'][i].strip()))
-    except KeyboardInterrupt:
-      sys.exit()
-    except:
-      logging.warning(f'Failed to tokenize text with id {data["id"][i]}. Skipping.')
+  with logging_redirect_tqdm():
+    for i in tqdm(range(len(data))):
+      try:
+        data['text'][i] = ' '.join(parser.tokenize(data['text'][i].strip()))
+      except KeyboardInterrupt:
+        sys.exit()
+      except:
+        logging.warning(f'Failed to tokenize text with id {data["id"][i]}. Skipping.')
 
   return data
 
 if __name__ == '__main__':
-  args = parser.parse_args()
+  logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO, datefmt='%H:%M:%S%p')
 
-  data = load_data(args.subset)
-  data = tokenize(data, args.server)
+  CORENLP_SERVER_URL = 'http://localhost:9000'
+  OUTPUT_FILE = 'wiki_tokenized.pickle'
+
+  parser = ArgumentParser()
+  parser.add_argument('subset', type=str, help='Subset of the wikipedia dataset to use (e.g. "en", "simple")')
+  parser.add_argument('--parser', type=str, metavar='URL', default=CORENLP_SERVER_URL, help='URL of the CoreNLP server to use (default: %(default)s)')
+  parser.add_argument('--output', '-o', type=str, metavar='OUTPUT_FILE', default=OUTPUT_FILE, help='Path to the output file (default: %(default)s)')
+  args = parser.parse_args()
+  
+  data = tokenize_wiki(args.subset, args.parser)
   
   logging.info('Saving...')
-  data.to_pickle(OUTPUT_FILE)
+  data.to_pickle(args.output)
 
